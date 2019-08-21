803e323f3a1f9b7df3508fb6a4af59f061c74da4
Hao Wu <gfphoenix78@gmail.com>
2019-08-21 11:05:24 +0800

    Fix script to extract tag from git repo

--- concourse/scripts/release_plr.sh
+++ concourse/scripts/release_plr.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_DIR=${CWDIR}/../../../
 pushd ${TOP_DIR}/plr_src
-PLR_VERSION=$(git describe | awk -F. '{printf("%d.%d.%d", $1, $2, $3)}')
+PLR_VERSION=$(git describe --tags | awk -F. '{printf("%d.%d.%d", $1, $2, $3)}')
 popd
 function release_gpdb() {
     case "$OSVER" in
