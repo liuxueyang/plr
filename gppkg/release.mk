0ce97ca1ad795a686fdd9d38c66816305e89832b
liuxueyang <liuxueyang457@gmail.com>
2022-08-11 10:51:00 +0800

    Fix PLR_OSS_VER
    In the opensource repo, version 8.3.1.15 does not exist.
    ref: https://github.com/postgres-plr/plr/tags

--- gppkg/release.mk
+++ gppkg/release.mk
@@ -1,5 +1,5 @@
 # Open source PLR version
-PLR_OSS_VER=8.3.1.15
+PLR_OSS_VER=8.3.0.16
 
 # Pivotal package version
 PLR_VER=3.0
@@ -8,4 +8,3 @@ PLR_REL=1
 # Embedded R version
 R_VER=3.3.3
 R_REL=1
-
